76a447d5b2c38c07879025b2219843aaf1b3d077
wtung008 <wayne.h.tung@gmail.com>
2017-02-07 23:10:26 -0500

    Creating a user model, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   # get 'products/index'
   #
   # get 'products/show'
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   # get 'products/edit'
 
   resources :products
+  resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206192948) do
+ActiveRecord::Schema.define(version: 20170208030655) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170206192948) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
